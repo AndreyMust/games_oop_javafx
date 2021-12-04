5392684e2c1d93e7d321e9eba2750d0e500f9e58
Andrew <andreymustafin.it@gmail.com>
2021-12-04 23:34:52 +0300

    2. Сокобан. Тестовое задание. [#75283 #235680] fix1

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,34 +1,34 @@
 package ru.job4j.puzzle;
 
 public class Win {
-    public static boolean checkVertical(int[][] board) {
+    public static boolean checkHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean checkVertical(int[][] board, int column) {
+        boolean result = true;
+        for (int[] ints : board) {
+            if (ints[column] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean check(int[][] board) { boolean isWin = true;
         for (int i = 0; i < board.length; i++) {
-            boolean isWin = true;
-            for (int j = 0; j < board.length; j++) {
-                isWin = isWin && (board[j][i] == 1);
-            }
-            if (isWin) {
-                return isWin;
+            if (checkHorizontal(board, i) || checkVertical(board, i)) {
+                return true;
             }
         }
         return false;
     }
-
-    public static boolean checkHorizontal(int[][] board) {
-        for (int row = 0; row < board.length; row++) {
-            boolean isWin = true;
-            for (int cell = 0; cell < board.length; cell++) {
-                isWin = isWin && (board[row][cell] == 1);
-            }
-            if (isWin) {
-                return isWin;
-            }
-        }
-        return false;
-    }
-
-    public static boolean check(int[][] board) {
-        return checkHorizontal(board) || checkVertical(board);
-    }
-
 }
