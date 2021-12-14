3a00c56e52816db85d1da0aac4bb96726ecfa7e1
Andrew <andreymustafin.it@gmail.com>
2021-12-15 00:16:29 +0300

    2. Каркас шахматной доски [#792] fix3

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -4,43 +4,46 @@ import org.junit.Test;
 import ru.job4j.chess.ImpossibleMoveException;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
 
 public class BishopBlackTest {
     @Test
     public void testIsDiagonalFalse() {
         BishopBlack black = new BishopBlack(Cell.D1);
-        assertThat(black.isDiagonal(Cell.D1, Cell.D3), is(false));
+        assertFalse(black.isDiagonal(Cell.D1, Cell.D3));
     }
 
     @Test
     public void testIsDiagonalTrue() {
         BishopBlack bishopBlack = new BishopBlack(Cell.C1);
-        assertThat(bishopBlack.isDiagonal(Cell.C1, Cell.D2), is(true));
-
+        assertTrue(bishopBlack.isDiagonal(Cell.C1, Cell.D2));
     }
 
     @Test
     public void testCopy() {
         BishopBlack black = new BishopBlack(Cell.A1);
         Figure figure = black.copy(Cell.C1);
-        assertThat(figure.position(), is(Cell.C1));
+        assertEquals(figure.position(), Cell.C1);
     }
 
     @Test
     public void testWayWhenC1ThenD2E3F4G5() {
         BishopBlack start = new BishopBlack(Cell.C1);
         Cell[] cells = start.way(Cell.H6);
-        assertThat(cells[0], is(Cell.D2));
-        assertThat(cells[1], is(Cell.E3));
-        assertThat(cells[2], is(Cell.F4));
-        assertThat(cells[3], is(Cell.G5));
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5, Cell.H6};
+        assertEquals(expected, cells);
     }
 
     @Test(expected = ImpossibleMoveException.class)
     public void testWayWhenC1ToG4ThenException() {
         BishopBlack start = new BishopBlack(Cell.C1);
         Cell[] cells = start.way(Cell.G4);
+    }
+
+    @Test
+    public void testPosition() {
+        BishopBlack pos = new BishopBlack(Cell.E1);
+        Cell tpos = pos.position();
+        assertEquals(tpos, Cell.E1);
     }
 }
